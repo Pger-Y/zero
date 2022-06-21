f271964f38ccdfec54c9d57a80621e0e5f16070f
John Smith <megman5576@gmail.com>
2022-06-21 16:08:14 +0800

    first run

--- main.tf
+++ main.tf
@@ -2,24 +2,9 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
+resource "aws_instance" "ec2" {
+  ami           = var.ec2_image
   instance_type = var.instance_type
 
   tags = {
--- variables.tf
+++ variables.tf
@@ -1,15 +1,14 @@
 variable "region" {
   description = "AWS region"
-  default     = "us-west-1"
+  default     = "ap-east-1"
 }
 
 variable "instance_type" {
   description = "Type of EC2 instance to provision"
-  default     = "t2.micro"
+  default     = "t3.micro"
 }
 
 variable "instance_name" {
   description = "EC2 instance name"
   default     = "Provisioned by Terraform"
 }
-
